f773ea8d8756a6ffa1c362cd340f7006510ba07b
Mohd Alquama <mohdalquama1@gmail.com>
2026-04-16 14:15:20 +0530

    mcv

--- Router/Home.Route.js
+++ Router/Home.Route.js
@@ -1,12 +1,17 @@
-import express from 'express';
+import express from "express";
 const HomeRouter = express.Router();
 
-HomeRouter.get('/', (req, res) => {
-    res.render('index');
+HomeRouter.get("/", (req, res) => {
+  res.render("index");
 });
 
-HomeRouter.get('/welcome', (req, res) => {
-    res.send('Welcome to the Home Page!');
+HomeRouter.get("/in", (req, res) => {
+  res.render("Check_In/index");
+});
+
+HomeRouter.post("/check-in", (req, res) => {
+  const { name, aadhar, phone, people, reason, start_date, end_date } = req.body;
+  res.render("Check_In/user_detail_enter", { people });
 });
 
 export default HomeRouter;
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import  express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 import HomeRouter from './Router/Home.Route.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
 
